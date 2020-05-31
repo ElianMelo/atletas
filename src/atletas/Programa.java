b8001154a85461b976d052e890ee4f25e9607b89
ElianMelo <elianmelomorais@gmail.com>
2020-05-30 21:44:40 -0300

    Leitura dos dados

--- src/atletas/Programa.java
+++ src/atletas/Programa.java
@@ -9,6 +9,30 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		String nome;
+		char sexo, outro;
+		Double altura, peso;
+		
+		do {
+			System.out.print("Nome: ");
+			nome = sc.nextLine();
+			do {
+				System.out.print("Sexo: ");
+				sexo = sc.next().toUpperCase().charAt(0);
+			} while (sexo != 'F' && sexo != 'M');
+			do {
+				System.out.print("Altura: ");
+				altura = sc.nextDouble();
+			} while(altura <= 0.0);
+			do {
+				System.out.print("Peso: ");
+				peso = sc.nextDouble();
+			} while(peso <= 0.0);
+			System.out.print("Digitar outro atleta (S/N)? ");
+			outro = sc.next().toUpperCase().charAt(0);
+			sc.nextLine();
+		} while(outro != 'N');
+		
 		sc.close();
 	}
 
